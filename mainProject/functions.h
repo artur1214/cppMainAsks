e2311fdb9ac2dc3df6bbfa3f3cad204f6fa3cbdf
artur1214 <41078064+artur1214@users.noreply.github.com>
2018-09-10 16:51:15 +0700

    Add files via upload

---
+++ mainProject/functions.h
@@ -0,0 +1,26 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#endif // FUNCTIONS_H
+#include <iostream>
+#include <time.h>
+#include <vector>
+
+class Game{
+    public:
+        std::vector<int> newNumber(){
+            std::vector<int> res;
+            srand(time(NULL));
+            std::vector<int> nums;
+            for(int i = 0; i< 10; i++){
+                nums.push_back(i);
+            }
+            for(int i = 0; i < 4; i++){
+                int pos = rand() % nums.size();
+                res.push_back(nums[pos]);
+                nums.erase(nums.begin() + pos);
+            }
+            return res;
+        }
+
+};
---
+++ mainProject/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "functions.h"
+using namespace std;
+
+
+
+int main()
+{
+
+    Game a;
+
+    cout << a.newNumber();
+
+
+    return 0;
+}
